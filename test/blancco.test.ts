c2a69fe71b8779eb932def8953cba3fecbf7572c
Kuivalainen <d.kuivalainen@yandex.ru>
2024-09-26 18:23:52 +0300

    Initial commit

---
+++ test/blancco.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Blancco from '../lib/blancco-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/blancco-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Blancco.BlanccoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
